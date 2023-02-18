471714d63392b63a5efed3096dc07f12a385b099
anniraz <zarinakudajberdikyzy@gmail.com>
2023-02-18 19:07:06 +0600

    doing courses

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets,permissions
+from rest_framework.response import Response
 
 from apps.courses.models import *
 from apps.courses.serializers import *
@@ -10,9 +11,23 @@ class CourseApiView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        my_courses=[i.course.id for i in CourseMembers.objects.filter(user=self.request.user)]
+        return Courses.objects.filter(pk__in=my_courses)
+
 
 
 class CourseMembersApiView(viewsets.ModelViewSet):
     queryset=CourseMembers.objects.all()
     serializer_class=CourseMemberSerializers
 
+    def create(self, request, *args, **kwargs):
+        course = request.data['course']
+        if course.owner==request.user:
+            return super().create(request,*args, **kwargs)
+        return Response({"ERROR":"You are not the owner"})
+            
+
+
+
+    
